e9ed9d9a6f36bfb48443b1c966694221a6045bb0
Dag Henrik Fjær <dhf@hydra.org>
2020-12-13 22:38:53 +0100

    Enable buttons

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.label import Label
 from kivy.uix.button import Button
+from kivy.uix.textinput import TextInput
 from kivy.uix.behaviors import FocusBehavior
 from kivy.uix.recycleview import RecycleView
 from kivy.uix.recyclegridlayout import RecycleGridLayout
@@ -12,44 +13,47 @@ from kivy.properties import ListProperty, BooleanProperty, ObjectProperty, Strin
 from kivy.uix.popup import Popup
 from kivy.graphics import Color, Rectangle
 
+from typing import final
+
+@final
 class TextInputPopup(Popup):
     obj = ObjectProperty(None)
     obj_text = StringProperty("")
 
     def __init__(self, obj, **kwargs):
-        super(TextInputPopup, self).__init__(kwargs)
+        super(TextInputPopup, self).__init__(**kwargs)
         self.obj = obj
         self.obj_text = obj.text
 
+        self.title = ""
         self.text_box = BoxLayout(orientation="vertical")
-        self.text_input = TextInput(id="textinput", text=self.obj_text)
-        self.save_button = Button(size_hint=(1, 0.2), text="Save changes",
-                                  on_release=save_btn_release)
-        self.cancel_button = Button(size_hint=(1, 0.2), text="Cancel changes", on_release=self.dismiss())
+        self.text_input = TextInput(text=self.obj_text)
+        self.save_button = Button(size_hint=(1, 0.2), text="Save changes")
+        self.save_button.bind(on_press=self.save_button_press)
+        self.cancel_button = Button(size_hint=(1, 0.2), text="Cancel changes")
+        self.cancel_button.bind(on_press=self.dismiss)
 
-        self.text_box.add_widget(text_input)
-        self.text_box.add_widget(save_button)
+        self.text_box.add_widget(self.text_input)
+        self.text_box.add_widget(self.save_button)
+        self.text_box.add_widget(self.cancel_button)
         self.add_widget(self.text_box)
 
-    def save_btn_release(instance):
+    def save_button_press(self, instance):
         self.obj.update_changes(self.text_input.text)
         self.dismiss()
 
+@final
 class SelectableRecycleGridLayout(FocusBehavior, LayoutSelectionBehavior,
                                   RecycleGridLayout):
     """Add selection and focus behavior to the view"""
 
     def __init__(self, **kwargs):
-        super(SelectableRecycleGridLayout, self).__init__(kwargs)
-        self.cols = 2
-        self.default_size = (None, dp(26))
-        self.default_size_hint: (1, None)
-        self.size_hint = None
-        self.height = self.minimum_height
-        self.orientation = "vertical"
-        self.multiselect = True
-        self.touch_multiselect = True
+        super(SelectableRecycleGridLayout, self).__init__(
+            default_size=(0, 28), default_size_hint=(1, None),
+            touch_multiselect=True, multiselect=True, cols=3,
+            height=self.minimum_height, **kwargs)
 
+@final
 class SelectableButton(RecycleDataViewBehavior, Button):
     """Add selection support to the Button"""
     index = None
@@ -89,6 +93,7 @@ class SelectableButton(RecycleDataViewBehavior, Button):
     def update_changes(self, txt):
         self.text = txt
 
+@final
 class Armory(BoxLayout):
     items = ListProperty([])
 
@@ -97,23 +102,25 @@ class Armory(BoxLayout):
         self.orientation = "vertical"
 
         # Item header bar
-        item_header = GridLayout(size_hint=(1,None), size_hint_y=None, height=25, cols=2)
+        item_header = GridLayout(size_hint=(1, None), size_hint_y=None, height=25, cols=3)
         item_name = Label(text="Name")
         item_notes = Label(text="Notes")
+        item_quality = Label(text="Quality")
         item_header.add_widget(item_name)
         item_header.add_widget(item_notes)
+        item_header.add_widget(item_quality)
 
         # Populate table
         self._get_items()
 
         # Item list
         item_list = BoxLayout()
-        recycle_view = RecycleView(data=[{"text": str(x)} for x in self.items])
-        #selectable_rgl = SelectableRecycleGridLayout()
-        #recycle_view.add_widget(selectable_rgl)
-        selectable_button = SelectableButton()
+        recycle_view = RecycleView()
+
+        recycle_view.add_widget(SelectableRecycleGridLayout())
+        recycle_view.data=[{"text": str(x)} for x in self.items]
         recycle_view.orientation = "vertical"
-        recycle_view.viewclass = selectable_button
+        recycle_view.viewclass = "SelectableButton"
         item_list.add_widget(recycle_view)
 
         self.add_widget(item_header)
@@ -121,14 +128,17 @@ class Armory(BoxLayout):
 
     def _get_items(self):
         # Temporary DB mock data
-        rows = [["foo", "bar"], ["baz", "foo"], ["bar", "baz"]]
+        data = [["foo", "bar"],
+                ["baz", "foo"],
+                ["bar", "baz"]]
 
-        for row in rows:
-            for col in row:
-                self.items.append(col)
+        for row in data:
+            for item in row:
+                self.items.append(item)
 
+@final
 class ArmoryApp(App):
-    title = "Armory"
+    title = "Armory v0.1"
 
     def build(self):
         return Armory()
